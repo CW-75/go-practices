1b74599b07539131d10a10341fa0364f155c33a7
CW-75 <cw75dev@gmail.com>
2025-09-15 17:47:09 -0400

    added variable example

---
+++ basic/variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var name string = "John"
+	var isEnabled bool = true
+	var pi float32 = 3.14
+	var doubleValue float64 = 2.71828
+
+	fmt.Println("Integer:", a)
+	fmt.Println("String:", name)
+	fmt.Println("Boolean:", isEnabled)
+	fmt.Println("Float:", pi)
+	fmt.Println("Double:", doubleValue)
+
+}
